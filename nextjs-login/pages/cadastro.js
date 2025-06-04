77d8ddd03864576ef0d5669387c2d8d2ce567324
Gabriela Borba <gborbas.gb@gmail.com>
2025-06-04 09:49:36 -0300

    Final Front

--- nextjs-login/pages/cadastro.js
+++ nextjs-login/pages/cadastro.js
@@ -1,10 +1,21 @@
 import LoginCard from "../scr/components/loginCard/loginCard"
-import style from "../styles/Cadastro.module.css"
+import style from "../styles/Login.module.css"
+import Input from "../scr/components/input/input"
+import Button from "../scr/components/button/button"
+import Link from 'next/Link'
 
 export default function CadastroPage() {
     return(
-        <div>
-            <LoginCard />
+        <div className={style.background}>
+            <LoginCard title="Crie sua conta">
+                <form className={style.form}>
+                    <Input type="text" placeholder="Seu nome"/>
+                    <Input type="email" placeholder="Seu e-mail"/>
+                    <Input type="password" placeholder="Sua senha"/>
+                    <Button>Cadastrar</Button>
+                    <Link href="/login">Possui conta?</Link>
+                </form>
+            </LoginCard>
         </div>
     )
 }
--- nextjs-login/pages/login.js
+++ nextjs-login/pages/login.js
@@ -1,10 +1,20 @@
 import LoginCard from "../scr/components/loginCard/loginCard"
 import styles from "../styles/Login.module.css"
+import Input from "../scr/components/input/input"
+import Button from "../scr/components/button/button"
+import Link from 'next/Link'
 
 export default function LoginPage() {
     return(
         <div className={styles.background}>
-            <LoginCard />
+            <LoginCard title="Entre em sua conta">
+                <form className={styles.form}>
+                    <Input type="email" placeholder="Seu e-mail"/>
+                    <Input type="password" placeholder="Sua senha"/>
+                    <Button>Entrar</Button>
+                    <Link href="/cadastro">Não possui conta?</Link>
+                </form>
+            </LoginCard>
         </div>
     )
 }
